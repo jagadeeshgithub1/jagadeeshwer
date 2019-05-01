a28050e8e3d38b5193438d8b16776d55d045f283
deepaveetil <deeparaveendran@adqura.com>
2019-05-01 18:33:23 +0530

    changes in action class openURL

--- README AutomationCheckList.txt
+++ README AutomationCheckList.txt
@@ -3,3 +3,4 @@
 1.Clear the IH 
 2.Ensure the engine status set to be 'Paused'(require to validate the csv)
 3.Ensure no other versions are checked out
+
--- src/main/java/driver/ActionClass.java
+++ src/main/java/driver/ActionClass.java
@@ -430,38 +430,67 @@ public class ActionClass extends TestBaseClass {
 		 */
 		// initialization();
 		boolean flag = false;
-		String osName = System.getProperty("os.name").trim();
+		String osName = System.getProperty("os.name");
+		System.out.println(osName);
 		String downloadFilepath = null;
-		try {
-			System.setProperty("webdriver.chrome.driver", "Drivers\\chromedriver.exe");
-			downloadFilepath = System.getProperty("user.dir") + "\\Downloads";
-			// System.setProperty("webdriver.chrome.driver", "Drivers/chromedriver");
-			// downloadFilepath = System.getProperty("user.dir") + "/Downloads";
-			if (osName.equalsIgnoreCase("Linux")) {
+		if (osName.equalsIgnoreCase("Linux")) {
+
+			try {
+				// System.setProperty("webdriver.chrome.driver", "Drivers/chromedriver);
+				// downloadFilepath = System.getProperty("user.dir") + "\\Downloads";
+				// System.setProperty("webdriver.chrome.driver", "Drivers/chromedriver");
+				// downloadFilepath = System.getProperty("user.dir") + "/Downloads";
 				System.setProperty("webdriver.chrome.driver", "Drivers/chromedriver");
 				downloadFilepath = System.getProperty("user.dir") + "/Downloads";
+
+				System.out.println("download path " + downloadFilepath);
+
+				// flag;
+			} catch (Exception e) {
+				flag = false;
+				return flag;
+
+				// TODO: handle exception
 			}
-			System.out.println("download path " + downloadFilepath);
-
-			// String downloadFilepath = prop.getProperty("DOWNLOADPATH");
-
-			HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
-			chromePrefs.put("profile.default_content_settings.popups", 0);
-			chromePrefs.put("download.default_directory", downloadFilepath);
-			ChromeOptions options = new ChromeOptions();
-			options.setExperimentalOption("prefs", chromePrefs);
-			driver = new ChromeDriver(options);
-
-			driver.manage().window().maximize();
-
-			driver.manage().deleteAllCookies();
-			driver.get(prop.getProperty("url"));
-			flag = true;
-		} catch (Exception e) {
-			flag = false;
-			// TODO: handle exception
 		}
+
+		else {
+
+			try {
+				System.setProperty("webdriver.chrome.driver", "Drivers\\chromedriver.exe");
+				downloadFilepath = System.getProperty("user.dir") + "\\Downloads";
+				// System.setProperty("webdriver.chrome.driver", "Drivers/chromedriver");
+				// downloadFilepath = System.getProperty("user.dir") + "/Downloads";
+				if (osName.equalsIgnoreCase("Linux")) {
+					System.setProperty("webdriver.chrome.driver", "Drivers/chromedriver");
+					downloadFilepath = System.getProperty("user.dir") + "/Downloads";
+				}
+				System.out.println("download path " + downloadFilepath);
+
+				// String downloadFilepath = prop.getProperty("DOWNLOADPATH");
+			} catch (Exception e) {
+
+				flag = false;
+				return flag;
+				// TODO: handle exception
+			}
+		}
+
+		HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
+		chromePrefs.put("profile.default_content_settings.popups", 0);
+		chromePrefs.put("download.default_directory", downloadFilepath);
+		ChromeOptions options = new ChromeOptions();
+		options.setExperimentalOption("prefs", chromePrefs);
+		driver = new ChromeDriver(options);
+
+		driver.manage().window().maximize();
+
+		driver.manage().deleteAllCookies();
+		driver.get(prop.getProperty("url"));
+		flag = true;
+
 		return flag;
+
 	}
 
 	public boolean quitBrowser() {
