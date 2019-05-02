0f0dbe22bfe770de7706bae20d9d6581c15f3a3d
deepaveetil <deeparaveendran@adqura.com>
2019-05-02 15:55:05 +0530

    new chckin after syncing the chromedriver and browser

--- src/main/java/driver/ActionClass.java
+++ src/main/java/driver/ActionClass.java
@@ -456,10 +456,12 @@ public class ActionClass extends TestBaseClass {
 			options.setExperimentalOption("prefs", chromePrefs);
 
 			// added the below 2 lines on 5/2/19
-			options.addArguments("--no-sandbox");
-			options.addArguments("--disable-dev-shm-usage");
+			/*
+			 * options.addArguments("--no-sandbox");
+			 * options.addArguments("--disable-dev-shm-usage");
+			 */
 			try {
-				driver = new ChromeDriver(options);
+				driver = new ChromeDriver(options);// some exception is coming hre
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				System.out.println("while driver = new ChromeDriver(options)");
