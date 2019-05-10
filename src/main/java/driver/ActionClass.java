79e6f59bc0c11f030f895ddae4d2f8cc05ee078d
deepaveetil <deeparaveendran@adqura.com>
2019-05-10 22:11:06 +0530

    chromedriver

--- src/main/java/driver/ActionClass.java
+++ src/main/java/driver/ActionClass.java
@@ -637,8 +637,8 @@ public class ActionClass extends TestBaseClass {
 		try {
 
 			if (osName.equalsIgnoreCase("Linux")) {
-				//System.setProperty("webdriver.chrome.driver", "/bin/chromedriver"); // added the new path for linux
-				System.setProperty("webdriver.chrome.driver", "Drivers/chromedriver");
+				System.setProperty("webdriver.chrome.driver", "/bin/chromedriver"); // added the new path for linux
+				//System.setProperty("webdriver.chrome.driver", "Drivers/chromedriver");
 				downloadFilepath = System.getProperty("user.dir") + "/Downloads";
 			} else {
 				System.setProperty("webdriver.chrome.driver", "Drivers\\chromedriver.exe");
